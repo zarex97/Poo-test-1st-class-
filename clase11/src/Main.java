40f60cc1ce1f698e199fa8201d70994730484beb
zarex97 <zarexlibertad@gmail.com>
2023-04-24 10:01:33 -0300

    feat: add player class

--- clase11/src/Main.java
+++ clase11/src/Main.java
@@ -1,3 +1,42 @@
+class Plater {
+    private int number;
+    private String name;
+    private String position;
+    private int goalCounter;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public int getGoalCounter() {
+        return goalCounter;
+    }
+
+    public void setGoalCounter(int goalCounter) {
+        this.goalCounter = goalCounter;
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
