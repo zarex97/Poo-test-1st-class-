f22f334186944ce8d86a09c9967a5bc8dd27bd02
zarex97 <zarexlibertad@gmail.com>
2023-04-24 10:07:19 -0300

    add player class

--- clase11/src/Main.java
+++ clase11/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 class Player {
     private int number;
     private String name;
@@ -37,6 +39,33 @@ class Player {
     }
 }
 
+class Team {
+    private String name;
+    private ArrayList<Player> players;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Player> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(ArrayList<Player> players) {
+        this.players = players;
+    }
+    public void addPlayer(Player player){
+
+    }
+
+    public void removePlayer(Player player){
+
+    }
+}
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
